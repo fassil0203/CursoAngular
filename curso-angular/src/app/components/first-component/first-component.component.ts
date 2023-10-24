41c4c1bbbc7b9891a68340b91d5fa24ab5b45a67
Fabio <fassil0203@gmail.com>
2023-10-24 13:42:52 -0300

    Atualizando

--- curso-angular/src/app/components/first-component/first-component.component.ts
+++ curso-angular/src/app/components/first-component/first-component.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class FirstComponentComponent implements OnInit{
   name = 'Fabio Santana';
   age :number = 33;
-  job = 'Programador'
+  job = 'Analista'
   Hobbies = ['Correr','Jogar','Estudar'];
   car ={
     name:'Polo',
