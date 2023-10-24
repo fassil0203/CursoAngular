1ef030d2f8179c960e8a70fbf1d0f2bc9ff99247
Fabio <fassil0203@gmail.com>
2023-10-24 06:07:18 -0300

    Curso Angular

---
+++ curso-angular/src/app/components/first-component/first-component.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first-component',
+  templateUrl: './first-component.component.html',
+  styleUrls: ['./first-component.component.css']
+})
+export class FirstComponentComponent implements OnInit{
+  name = 'Fabio Santana';
+  age :number = 33;
+  job = 'Programador'
+  Hobbies = ['Correr','Jogar','Estudar'];
+  car ={
+    name:'Polo',
+    year:2022,
+  }
+
+  construtor(){}
+  ngOnInit(): void {
+      
+  }
+}
+
+
